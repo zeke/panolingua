8aa81332c0e034ec38fb59c6c02e4e4acd06a5f2
zeke <zeke@sikelianos.com>
2012-04-16 17:22:55 -0700

    vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panolingua::Application.config.secret_token = '262676731b650b90f86694a7dda1c367f4bd270a41ad11f86095e5640c07635d65326fe87f3eb46dea2e6f24124d7182e12954b91cbb828f989d86e4a4d5571c'
